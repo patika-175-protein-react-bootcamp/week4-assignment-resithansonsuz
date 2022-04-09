dc8273d68894d4da6baddec899bdd53e15e57a18
Ömer Reşithan Sonsuz <45916117+resithansonsuz@users.noreply.github.com>
2022-04-09 22:39:01 +0300

    Update rooter.js

--- src/rooter/rooter.js
+++ src/rooter/rooter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Start from "../pages/startPage";
 import QuestionsPage from "../pages/questionsPage";
+import ConslusionPage from "../pages/conclusionPage";
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
     <Routes>
       <Route path="/" element={<Start />} />
       <Route path="questionsPage" element={<QuestionsPage />} />
+        <Route path="conclusionPage" element={<ConslusionPage />} />
     </Routes>
   );
 }
